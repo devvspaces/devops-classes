3d6c85d2388487ae597055fa56906e3fcbfae595
Ayanwola Ayomide <77179231+devvspaces@users.noreply.github.com>
2025-02-20 18:10:00 +0100

    first commit

---
+++ Week 1/Python/main.py
@@ -0,0 +1,68 @@
+
+# # Tuples
+# numbers = (1, 2, 3, 4, 5)
+# print(numbers)
+
+# # List
+# numbers = [1, 2, 3, 4, 5]
+# numbers[0] = 10
+# print(numbers)
+
+# # Set
+# numbers = {1, 2, 3, 4, 5, 5}
+# print("Set", numbers)
+
+# # Range
+# print(list(range(0, 6, 2)))
+
+# # Arithmetic operators
+# x = 5
+# y = 4
+# z = x * y
+# z = x / y
+# z = x ** x
+# z = x % y
+# z = x // y
+# z = x + y
+# z = x - y
+# print(z)
+
+# # Assignment operators
+# a = 4
+
+# # Comparison operators
+# m = 6
+# n = 10
+# print(m > n)
+# print(m < n)
+# print(m >= n)
+# print(m <= n)
+# print(m == n)
+
+# # Logical operators
+# print(m > n and m < n)
+# print(m > n or m < n)
+# print(not(m > n and m < n))
+
+# Identity operators
+g = [4]
+h = [4]
+print(g is h)
+print(g is not h)
+
+# Membership operators
+zenith = ["ayo", "tolu", "kenny"]
+print("ayo" in zenith)
+print("Ayo" not in zenith)
+
+# Strings
+name = "Ayomide is {} {}"
+print(name.format("nice", "nicer"))
+print(name.upper())
+
+# Slices
+
+
+# List
+zenith.append("Christiana")
+print(zenith)
